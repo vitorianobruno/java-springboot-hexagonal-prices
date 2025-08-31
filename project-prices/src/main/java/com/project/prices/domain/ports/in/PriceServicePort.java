041c4274550924782bbe5614a568aefe657b6d23
Bruno Vitoriano <vitorianobruno@gmail.com>
2025-08-31 10:02:51 +0200

    Update PriceServicePort.java

--- project-prices/src/main/java/com/project/prices/domain/ports/in/PriceServicePort.java
+++ project-prices/src/main/java/com/project/prices/domain/ports/in/PriceServicePort.java
@@ -1,13 +1,8 @@
 package com.project.prices.domain.ports.in;
 
 import com.project.prices.domain.models.Price;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 
-@Service
 public interface PriceServicePort {
-
-    ResponseEntity<Price> getPrice(LocalDateTime start_date, int product_id, int brand_id);
+    Price getPrice(LocalDateTime start_date, int product_id, int brand_id);
 }
